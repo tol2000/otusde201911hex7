724147d62d4eba2191a2f3b8e6120fbfc04bb1cf
tol2000 <tolicheg@gmail.com>
2019-12-31 03:17:03 +0200

    ---

--- src/main/scala/org/kliusa/otusde201911hex7/safetyboston/SafetyBoston.scala
+++ src/main/scala/org/kliusa/otusde201911hex7/safetyboston/SafetyBoston.scala
@@ -83,6 +83,25 @@ object SafetyBoston extends App {
 
 */
 
+  case class ClassTot(
+    INCIDENT_NUMBER:String, OFFENSE_CODE:Int, OFFENSE_CODE_GROUP:String, OFFENSE_DESCRIPTION:String,
+    DISTRICT:String, REPORTING_AREA:String, SHOOTING:String, OCCURRED_ON_DATE:String,
+    YEAR:Int, MONTH:Int, DAY_OF_WEEK:String, HOUR:Int, UCR_PART:String, STREET:String,
+    Lat:Double, Long:Double, Location:String
+  )
 
+  val dsTot = crimeDs.as[ClassTot]
+      .groupBy("DISTRICT").
+    //.count()
+
+//    .agg(
+//      Map(
+//        "*" -> "count",
+//        "Lat" -> "avg",
+//        "Long" -> "avg"
+//      )
+//    )
+
+  dsTot.show(100,100)
 
 }
